fef13f8c5b81768edecfae4a9c6c4430f952afc1
numraise <num@raiseinnovation.com>
2022-07-25 23:45:50 +0700

    add sand

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 player.onChat("com", function () {
     agent.move(FORWARD, 5)
     mobs.spawn(CHICKEN, player.position())
+    blocks.place(RED_SAND, player.position())
     agent.destroy(FORWARD)
     agent.collectAll()
     agent.teleportToPlayer()
