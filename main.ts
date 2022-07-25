c4b3b7965298f9823a51e67d156e0cc6c6374de7
numraise <num@raiseinnovation.com>
2022-07-25 23:40:24 +0700

    add chicken

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 player.onChat("com", function () {
     agent.move(FORWARD, 5)
+    mobs.spawn(CHICKEN, player.position())
     agent.destroy(FORWARD)
     agent.collectAll()
     agent.teleportToPlayer()
